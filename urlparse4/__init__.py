0cad122008e67bc190d84189571d7c09aa056249
nctl144 <nctl144@gmail.com>
2018-06-06 17:37:12 -0500

    import urlparse in py3

--- urlparse4/__init__.py
+++ urlparse4/__init__.py
@@ -1,6 +1,7 @@
 # https://github.com/python/cpython/blob/2.7/Lib/urlparse.py
 
-from urlparse import *
+import six
+from six.moves.urllib.parse import urlparse, urlsplit, urljoin
 
 _original_urlsplit = urlsplit
 _original_urljoin = urljoin
