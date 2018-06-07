9c8a185323bcdd2a78a09ac2d08b5a64dc965a01
nctl144 <nctl144@gmail.com>
2018-06-07 11:29:20 -0500

    fix the import error

--- urlparse4/__init__.py
+++ urlparse4/__init__.py
@@ -1,7 +1,6 @@
 # https://github.com/python/cpython/blob/2.7/Lib/urlparse.py
 
 import six
-import pyximport
 
 from six.moves.urllib.parse import urlparse, urlsplit, urljoin
 
@@ -9,5 +8,4 @@ from six.moves.urllib.parse import urlparse, urlsplit, urljoin
 _original_urlsplit = urlsplit
 _original_urljoin = urljoin
 
-pyximport.install()
-from urlparse4.cgurl import url_split, url_join
+from cgurl import url_split, url_join
