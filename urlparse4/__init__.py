3d7eb0e3a5b417a44715464a29d8a7d4dc708d1a
nctl144 <nctl144@gmail.com>
2018-06-11 14:10:12 -0500

    fix failing test by importing

--- urlparse4/__init__.py
+++ urlparse4/__init__.py
@@ -1,11 +1,10 @@
-# https://github.com/python/cpython/blob/2.7/Lib/urlparse.py
-
 import six
 
 if six.PY2:
     from urlparse import *
 else:
     from urllib.parse import *
+    from urllib.parse import Quoter, _ALWAYS_SAFE
 
 
 _original_urlsplit = urlsplit
