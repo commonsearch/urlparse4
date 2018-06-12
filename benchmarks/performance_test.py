4165c4f537f763fda287a18d1fd631e5abd6a13a
nctl144 <nctl144@gmail.com>
2018-06-12 11:03:45 -0500

    update the performance test

--- benchmarks/performance_test.py
+++ benchmarks/performance_test.py
@@ -1,33 +1,78 @@
 from urlparse4 import urlsplit, urljoin
 from timeit import default_timer as timer
 
-total = 0
+from sys import argv
 
-with open('urls/chromiumUrls.txt') as f:
-    for url in f:
+encode = False
 
-        start = timer()
+try:
+    if argv[1] == "encode":
+        encode = True
+except IndexError:
+    print("encode is not defined, continue with the program...")
 
-        a = urlsplit(url.encode())
+if encode:
+    total = 0
 
-        end = timer()
+    for i in range(50):
+        with open('urls/chromiumUrls.txt') as f:
+            for url in f:
 
-        total += end - start
+                start = timer()
 
-print("the urlsplit time is", total, "seconds")
+                a = urlsplit(url.encode())
+
+                end = timer()
+
+                total += end - start
+
+    print("the urlsplit time with encode in python is", total / 50, "seconds")
 
 
-total2 = 0
+    total2 = 0
 
-with open('urls/chromiumUrls.txt') as f:
-    for url in f:
+    for i in range(50):
+        with open('urls/chromiumUrls.txt') as f:
+            for url in f:
 
-        start = timer()
+                start = timer()
 
-        a = urljoin(url.encode(), "/asd".encode())
+                a = urljoin(url.encode(), "/asd".encode())
 
-        end = timer()
+                end = timer()
 
-        total2 += end - start
+                total2 += end - start
 
-print("the urljoin time is", total2, "seconds")
+    print("the urljoin time with encode in python is", total2 / 50, "seconds")
+
+else:
+    total = 0
+    for i in range(50):
+        with open('urls/chromiumUrls.txt') as f:
+            for url in f:
+
+                start = timer()
+
+                a = urlsplit(url)
+
+                end = timer()
+
+                total += end - start
+
+    print("the urlsplit time without encoding in python is", total / 50, "seconds")
+
+
+    total2 = 0
+    for i in range(50):
+        with open('urls/chromiumUrls.txt') as f:
+            for url in f:
+
+                start = timer()
+
+                a = urljoin(url, "/asd")
+
+                end = timer()
+
+                total2 += end - start
+
+    print("the urljoin time without encoding in python is", total2 / 50, "seconds")
